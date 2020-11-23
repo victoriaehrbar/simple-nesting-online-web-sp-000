6063495fbc8f3194a7c2149f7e85492acf1a3c86
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-23 03:23:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,7 @@ def adding_matz
  		{:yukihiro_matsumoto => {
       :known_for => "Ruby",
        :languages => ["LISP", "C"]
-    
+
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -83,7 +83,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-}
 
 end
 
