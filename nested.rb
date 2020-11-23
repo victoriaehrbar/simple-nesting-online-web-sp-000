74ba0e8323a20d09d496ca479342b8ba81e70d4b
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-23 03:26:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
      alans_new_info = "GUI"
 
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-	programmer_hash
+	   programmer_hash
 
 
 end
@@ -130,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     	programmer_hash
 end
